94e2584f2b3fdb8132b766eff7600c8605b5985e
Max Jilu Wang <wangjilu23@gmail.com>
2022-02-24 01:38:42 +1100

    populate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant::CATEGORY.each do |category|
+  5.times do
+    Restaurant.create( {name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: category })
+  end
+end
